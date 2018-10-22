c2271f9cf56e8cd378800523e498c1e873286d92
Gustavo Santos <gustavo.dev@outlook.com.br>
2018-10-22 19:38:25 -0300

    style(global): refatorando namespaces do projeto InsurSoft.Backend.Web.Api

--- src/InsurSoft.Backend.Web.Api/Controllers/PingController.cs
+++ src/InsurSoft.Backend.Web.Api/Controllers/PingController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace InsurSoft.Backend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/web/[controller]")]
     [ApiController]
     public class PingController : ControllerBase
     {
@@ -18,3 +13,4 @@ namespace InsurSoft.Backend.Controllers
         }
     }
 }
+
