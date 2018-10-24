24ba044c69131f615c35c10a635214afcb624c91
Gustavo Santos <gustavo.dev@outlook.com.br>
2018-10-24 01:11:29 -0300

    feat(Segurados): criação inicial do modelo de segurado.
    Adicionada biblioteca FunctionalExtensions
    Criação do ValueObject NomeCompleto

---
+++ src/InsurSoft.Backend.Web.Segurados.Domain/Entities/Segurado.cs
@@ -0,0 +1,13 @@
+﻿using InsurSoft.Backend.Web.Segurados.Domain.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsurSoft.Backend.Web.Segurados.Domain.Entities
+{
+    public class Segurado
+    {
+        public Guid Id { get; private set; }
+        public NomeCompleto Nome { get; private set; }
+    }
+}
---
+++ src/InsurSoft.Backend.Web.Segurados.Domain/Types/NomeCompleto.cs
@@ -0,0 +1,45 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsurSoft.Backend.Web.Segurados.Domain.Types
+{
+    public class NomeCompleto : ValueObject
+    {
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+
+        private NomeCompleto(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        public static Result<NomeCompleto> Create(Maybe<string> nome, Maybe<string> sobrenome)
+        {
+            var resultNome = nome
+                .ToResult("O nome não deve ser vazio")
+                .Ensure(n => n.Length >= 3, "O nome deve ter ao menos 3 caracteres")
+                .Ensure(n => n.Length <= 20, "O nome deve ter no máximo 20 caracteres");
+
+            var resultSobrenome = sobrenome
+                .ToResult("O sobrenome não deve ser vazio")
+                .Ensure(n => n.Length >= 3, "O sobrenome deve ter ao menos 3 caracteres")
+                .Ensure(n => n.Length <= 50, "O sobrenome deve ter no máximo 50 caracteres");
+
+            var result = Result.Combine(resultNome, resultSobrenome);
+
+            if (result.IsFailure)
+                return Result.Fail<NomeCompleto>(result.Error);
+
+            return Result.Ok(new NomeCompleto(nome.Value, sobrenome.Value));
+        }
+        
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Nome;
+            yield return Sobrenome;
+        }
+    }
+}
